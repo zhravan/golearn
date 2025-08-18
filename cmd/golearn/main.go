1e2a13a43a7adb3733e26341987306a66bac3ab0
zhravan <mrshravankumarb@gmail.com>
2025-08-18 15:27:34 +0530

    feat(cli): add entrypoint to invoke internal CLI

---
+++ cmd/golearn/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"golearn/internal/cli"
+)
+
+func main() {
+	// Allow -h without subcommands to show help
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, cli.HelpText())
+	}
+
+	if err := cli.Execute(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
