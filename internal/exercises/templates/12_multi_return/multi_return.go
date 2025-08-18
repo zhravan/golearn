03012836a8116863d2421086fd25240ad0cfba12
zhravan <mrshravankumarb@gmail.com>
2025-08-19 04:34:51 +0530

    feat(templates/12_multi_return): add multi_return.go

---
+++ internal/exercises/templates/12_multi_return/multi_return.go
@@ -0,0 +1,9 @@
+package multireturn
+
+import "fmt"
+
+// TODO: Divide a by b, returning quotient, remainder, and error for divide-by-zero
+func DivMod(a, b int) (int, int, error) {
+	_ = fmt.Errorf
+	return 0, 0, nil
+}
