04b188ffe0c20d0ba796bcd6fc8e449782242184
zhravan <mrshravankumarb@gmail.com>
2025-08-19 04:34:51 +0530

    feat(templates/02_values): add values.go

---
+++ internal/exercises/templates/02_values/values.go
@@ -0,0 +1,7 @@
+package values
+
+// TODO: Implement FormatValues to return a formatted string combining i, f, and s
+func FormatValues(i int, f float64, s string) string {
+	// change this to something like: fmt.Sprintf("i=%d f=%.2f s=%s", ...)
+	return ""
+}
