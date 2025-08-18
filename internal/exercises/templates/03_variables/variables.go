57f97817d890108b406cc21a88b0c4333dfa07ad
zhravan <mrshravankumarb@gmail.com>
2025-08-19 04:34:51 +0530

    feat(templates/03_variables): add variables.go

---
+++ internal/exercises/templates/03_variables/variables.go
@@ -0,0 +1,7 @@
+package variables
+
+// TODO: Return two variables (name and age) initialized in function scope
+func MakePerson() (string, int) {
+	// Use short variable declarations
+	return "", 0
+}
