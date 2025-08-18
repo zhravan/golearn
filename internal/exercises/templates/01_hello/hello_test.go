d35f5b8d9f17a0aefc9cc749a983221ff45ed07c
zhravan <mrshravankumarb@gmail.com>
2025-08-18 15:27:35 +0530

    test(templates): add test for Hello() expected output

---
+++ internal/exercises/templates/01_hello/hello_test.go
@@ -0,0 +1,11 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, Go!"
+	if got != want {
+		t.Fatalf("Hello() = %q, want %q", got, want)
+	}
+}
