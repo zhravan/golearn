4f7f46dc98cc07814f9cee560bc95a31f30876e3
thearpankumar <arpankumar1119@outlook.com>
2025-10-02 18:47:11 +0530

    fix: add negative input guard to factorial function

--- internal/exercises/solutions/15_recursion/recursion.go
+++ internal/exercises/solutions/15_recursion/recursion.go
@@ -2,6 +2,9 @@ package recursion
 
 // factorial returns the factorial of n using recursion
 func factorial(n int) int {
+	if n < 0 {
+		panic("factorial: negative input not supported")
+	}
 	if n == 0 {
 		return 1
 	}
