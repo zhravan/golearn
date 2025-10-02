4cc01fb8a54a4eb4a843e68619b7f6b72ace3337
Joan Jara Bosch <jaraboschj@gmail.com>
2025-10-02 14:48:14 +0200

    Create regular-expressions_solution.go

---
+++ internal/exercises/solution/regular-expressions_solution.go
@@ -0,0 +1,14 @@
+// regular-expressions_solution.go
+// Solution for: Regular Expressions
+package solution
+
+import "regexp"
+
+// MatchRegex returns true if the input matches the pattern, false otherwise.
+func MatchRegex(pattern, input string) bool {
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return false
+	}
+	return re.MatchString(input)
+}
