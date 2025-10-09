cef227ee7de6339833044aada68be17ec2ff462f
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-10-09 17:04:21 +0530

    Added Solution for text-templates exercises

--- internal/exercises/templates/40_text_templates/text_templates.go
+++ internal/exercises/templates/40_text_templates/text_templates.go
@@ -7,25 +7,10 @@ import (
 
 // RenderTemplate takes a template string and data map, returns the rendered string or error.
 func RenderTemplate(tmplStr string, data map[string]any) (string, error) {
-	tmpl, err := template.New("exercise").Parse(tmplStr)
-	if err != nil {
-		return "", err
-	}
-	
-	var buf bytes.Buffer
-	err = tmpl.Execute(&buf, data)
-	if err != nil {
-		return "", err
-	}
-	
-	return buf.String(), nil
+	return "", nil
 }
 
 // FormatUserGreeting returns a greeting using text/template with user's name
 func FormatUserGreeting(name string) (string, error) {
-	const greetingTmpl = "Hello, {{.Name}}!"
-	data := map[string]any{
-		"Name": name,
-	}
-	return RenderTemplate(greetingTmpl, data)
+	return "", nil
 }
