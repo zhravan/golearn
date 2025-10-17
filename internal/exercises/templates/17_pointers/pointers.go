665a7a24758feb65c4629aed45a15abbc5cb6594
Sidharth chauhan <chauhansiddharth71@gmail.com>
2025-10-17 17:24:37 +0530

    Add 17_pointers solution and restore template

--- internal/exercises/templates/17_pointers/pointers.go
+++ internal/exercises/templates/17_pointers/pointers.go
@@ -1,9 +1,9 @@
 package pointers
 
+// TODO: Modify the value pointed to by the incoming *int.
+// Return the same pointer after updating the underlying value.
+// Avoid allocating any new memory; operate in place.
 func modifyValue(ptr *int) *int {
-	if ptr == nil {
-		return nil
-	}
-	*ptr = 100
+	// TODO: implement mutation via pointer
 	return ptr
 }
