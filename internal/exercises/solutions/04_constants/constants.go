cd7288bd9c74626c5c907d86d376169fb29462f9
AmandeepMandal1077 <2023kucp1077@iiitkota.ac.in>
2025-10-28 11:48:29 +0530

    added comment

--- internal/exercises/solutions/04_constants/constants.go
+++ internal/exercises/solutions/04_constants/constants.go
@@ -2,6 +2,7 @@ package constants
 
 import "math"
 
+// CircleArea calculates and returns the area of a circle with the given radius.
 func CircleArea(r float64) float64 {
 	return math.Pi * r * r
 }
