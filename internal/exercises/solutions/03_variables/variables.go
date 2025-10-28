b41062ec7553ab6a20fd80355b6b1d4b15700a12
AmandeepMandal1077 <2023kucp1077@iiitkota.ac.in>
2025-10-28 11:46:44 +0530

    added comment

--- internal/exercises/solutions/03_variables/variables.go
+++ internal/exercises/solutions/03_variables/variables.go
@@ -1,5 +1,6 @@
 package variables
 
+// MakePerson returns two variables (name and age) initialized in function scope
 func MakePerson() (string, int) {
 	// Use short variable declarations
 	name := "Gopher"
