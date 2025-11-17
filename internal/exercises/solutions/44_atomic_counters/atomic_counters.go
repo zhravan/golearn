e7c77cf6f16e799fb5db825ff7cb4b73791f03d3
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2025-11-17 14:20:22 +0530

    Add atomic counter exercise

---
+++ internal/exercises/solutions/44_atomic_counters/atomic_counters.go
@@ -0,0 +1,22 @@
+package atomic_counters
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+func IncrementCounter() uint64 {
+
+	var ops atomic.Uint64
+	var wg sync.WaitGroup
+
+	wg.Add(10_000)
+	for range 10_000 {
+		go func() {
+			defer wg.Done()
+			ops.Add(1)
+		}()
+	}
+	wg.Wait()
+	return ops.Load()
+}
---
+++ internal/exercises/templates/44_atomic_counters/atomic_counters.go
@@ -0,0 +1,7 @@
+package atomic_counters
+
+// TODO: 1. Launch 10_000 goroutines that increment a counter.
+//  2. Wait for all goroutines to finish and return the final value of the counter.
+func IncrementCounter() uint64 {
+	return 0
+}
