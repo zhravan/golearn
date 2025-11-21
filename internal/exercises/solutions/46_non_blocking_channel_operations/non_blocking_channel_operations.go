5982971b9e6c2b10ae677631a51c50b2f2d486df
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2025-11-21 14:15:28 +0530

    Add non blocking channel operation exercise.

---
+++ internal/exercises/solutions/46_non_blocking_channel_operations/non_blocking_channel_operations.go
@@ -0,0 +1,66 @@
+package non_blocking_channel_operations
+
+import (
+	"sync"
+	"time"
+)
+
+// Assume case where you have high throughput system
+// where you have to drop logs
+// if they cannot be processed in time
+// in order to keep system throughput high.
+
+type Logger struct {
+	ch        chan string
+	wg        sync.WaitGroup
+	processed int
+	dropped   int
+
+	mu sync.Mutex
+}
+
+func NewLogger(bufferSize int) *Logger {
+	l := &Logger{
+		ch: make(chan string, bufferSize),
+	}
+	l.wg.Add(1)
+	go l.run()
+	return l
+}
+
+var processingDelay = 200 * time.Microsecond
+
+// slow log processor
+func (l *Logger) run() {
+	defer l.wg.Done()
+	for msg := range l.ch {
+		_ = msg
+		time.Sleep(processingDelay)
+
+		l.mu.Lock()
+		l.processed++
+		l.mu.Unlock()
+	}
+}
+
+func (l *Logger) Log(msg string) {
+	select {
+	case l.ch <- msg: // queued
+	default:
+		l.mu.Lock()
+		l.dropped++
+		l.mu.Unlock()
+	}
+}
+
+// graceful shutdown
+func (l *Logger) Close() {
+	close(l.ch)
+	l.wg.Wait()
+}
+
+func (l *Logger) Stats() (processed, dropped int) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.processed, l.dropped
+}
